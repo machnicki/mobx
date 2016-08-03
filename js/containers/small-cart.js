a43e26e4d8ab1b80467fa17e0cc3f0bcc9b66e3e
Miron Machnicki <miron@machnicki.com.pl>
2016-08-03 21:19:03 +0100

    Animation on cart

--- js/containers/small-cart.js
+++ js/containers/small-cart.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react'
 import store from '../store'
+import { autorun } from 'mobx'
 
 @observer
 class SmallCart extends Component {
+  previousQuantity = store.itemsInCartQuantity
+  timeout
+
+  constructor(props) {
+    super(props)
+    this.state = { isAnimated: false }
+
+    autorun(() => {
+      if (store.itemsInCartQuantity > this.previousQuantity) {
+        clearTimeout(this.timeout)
+        this.setState({ isAnimated: true })
+        this.timeout = setTimeout(() => this.setState({ isAnimated: false }), 500)
+      }
+      this.previousQuantity = store.itemsInCartQuantity
+    })
+  }
+
   render() {
     return (
-      <div className="small-cart">
+      <div className={`small-cart ${this.state.isAnimated ? 'is-animated' : ''}`}>
         Items in basket: <strong>{ store.itemsInCartQuantity }</strong>
       </div>
     )
